5a208aac0d34f5960166eea0ecf4ec5c2a9bcbdf
Sumnan Azadi <sumnanazadi@gmail.com>
2022-08-19 18:02:18 +0600

    validation added for phone number and plan
    - Check If the employee already has a primary phone number
    - Check If the employee already has a plan under certain company

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from employee.models import Employee, Phone
 
@@ -13,3 +14,34 @@ class PhoneSerializers(serializers.ModelSerializer):
     class Meta:
         model = Phone
         fields = '__all__'
+
+    def validate(self, attrs):
+        employee = attrs.get('employee')
+        is_primary = attrs.get('is_primary')
+        company = attrs.get('company')
+        plan = attrs.get('plan')
+
+        # If the phone is selected as primary, then make sure that there is only one primary phone for the employee.
+        if is_primary:
+            is_employee_has_primary_phone = Phone.objects.filter(employee=employee,
+                                                                 is_primary=True).values_list('phone',
+                                                                                              flat=True)
+            if is_employee_has_primary_phone:
+                msg = {"Primary_Phone": ["This user has already a primary number. Current primary number is {}".
+                                             format(is_employee_has_primary_phone[0])]}
+                raise ValidationError(msg)
+
+        # If plan is selected, then make sure that employee has not already a plan in the selected company.
+        elif plan:
+            is_employee_has_plan_in_company = Phone.objects.filter(employee=employee,
+                                                                   company=company,
+                                                                   plan__isnull=False)
+            employee_current_plan = Phone.objects.filter(employee=employee,
+                                                         company=company).values_list('plan__name',
+                                                                                      flat=True)
+            if is_employee_has_plan_in_company:
+                msg = {"Plan": ["This user has already a plan under {} company. Current plan is {}".
+                                    format(company, employee_current_plan[0])]}
+                raise ValidationError(msg)
+
+        return attrs
